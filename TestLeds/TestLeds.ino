04d58535ea5110fc091f9d0672e4e3387d91c626
DonaldNgai <donaldtak@hotmail.com>
2015-08-17 17:04:30 -0400

    Initial Commit

---
+++ TestLeds/TestLeds.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(0, OUTPUT);
+pinMode(1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(0, HIGH);
+digitalWrite(1, HIGH);
+}
